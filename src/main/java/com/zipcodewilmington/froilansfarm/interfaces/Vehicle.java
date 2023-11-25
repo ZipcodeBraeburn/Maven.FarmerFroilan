e5479573434bc7eb58f9b50fead3dceb77b10576
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-25 10:39:09 -0500

    vehicle extending noisemaker, rideable

--- src/main/java/com/zipcodewilmington/froilansfarm/interfaces/Vehicle.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/interfaces/Vehicle.java
@@ -1,4 +1,10 @@
 package com.zipcodewilmington.froilansfarm.interfaces;
 
-public interface Vehicle {
+import com.zipcodewilmington.froilansfarm.abstractClasses.Rider;
+
+public interface Vehicle extends NoiseMaker, Rideable<Rider>{
+    // Note: might have to make another interface for riders of man-made vehicles.
+    // i.e., pilot, farmer
+    // because horseRider can only ride horses, the <Rider> here is too vague
+    // it will include horseRiders as an acceptable Rider for Vehicles
 }
