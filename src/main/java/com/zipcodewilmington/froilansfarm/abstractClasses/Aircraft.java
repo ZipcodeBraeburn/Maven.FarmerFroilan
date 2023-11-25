b236be4f8787e04eb8126c26a7d752621734bcf1
Tam Maria <tammaria@Zipcoders-MacBook-Pro-8.local>
2023-11-25 14:11:40 -0500

    abstracts, turned crop into interface

--- src/main/java/com/zipcodewilmington/froilansfarm/abstractClasses/Aircraft.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/abstractClasses/Aircraft.java
@@ -1,6 +1,12 @@
 package com.zipcodewilmington.froilansfarm.abstractClasses;
 
-public abstract class Aircraft {
+import com.zipcodewilmington.froilansfarm.interfaces.Vehicle;
 
+public abstract class Aircraft extends Vehicle<Pilot> {
+
+    public boolean fly(){
+        System.out.println("Flying~~~");
+        return true;
+    }
 
 }
---
+++ src/main/java/com/zipcodewilmington/froilansfarm/interfaces/Crop.java
@@ -0,0 +1,7 @@
+package com.zipcodewilmington.froilansfarm.interfaces;
+import com.zipcodewilmington.froilansfarm.abstractClasses.Produce;
+import com.zipcodewilmington.froilansfarm.interfaces.EdibleCrops;
+
+public interface Crop<EdibleCropsType extends EdibleCrops> extends Produce<EdibleCropsType> {
+    public EdibleCropsType yield(boolean hasBeenFertilized, boolean hasBeenHarvested);
+}
